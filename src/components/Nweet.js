d7f32acf0f8b4c7347a710f98ce70fe1ddf7a8c5
All-For-OneTwo <scottyyari@hotmail.com>
2021-09-01 14:55:59 +0900

    5장 완료 커밋

---
+++ src/components/Nweet.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { dbService } from 'fbase';
+
+const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm('삭제하시겠습니까?');
+    if (ok) {
+      console.log(nweetObj.id);
+      const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      console.log(data);
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewNweet(value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.doc(`nweets/${nweetObj.id}`).update({ text: newNweet });
+    setEditing(false);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={newNweet} required />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>  
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Nweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,62 @@
-const Home = () => <span>Home</span>;
+import { useEffect, useState } from 'react';
+import { dbService } from 'fbase';
+import Nweet from 'components/Nweet';
+
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    dbService.collection('nweets').onSnapshot((snapshot) => {
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(newArray);
+    });
+  }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.collection('nweets').add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+    });
+    setNweet('');
+  };
+
+  const onChange = (e) => {
+    e.preventDefault();
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={nweet}
+          placeholder="What's on your mind?"
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
