013554166c8d95c2608f90ccf8d2e5a3643d4ae6
All-For-OneTwo <scottyyari@hotmail.com>
2021-09-01 12:42:56 +0900

    4장 완료 커밋

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
-import AppRouter from '../routes/Router';
+import { useEffect, useState } from 'react';
+import AppRouter from 'routes/Router';
+import { authService } from 'fbase';
 
 function App() {
-  return <AppRouter />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'initializing...'}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  )
 }
 
 export default App;
